60902370fa1ff9f3ad0379f93ebb4dd25758104f
mcoam <miguelcoam@gmail.com>
2018-05-30 17:55:55 -0400

    Print hasta mail id2

--- parser_zimbra.pl
+++ parser_zimbra.pl
@@ -8,7 +8,10 @@ my $message_id;
 my $message_size;
 my $message_time;
 my $nr_recipient;
-my $mail_id_status;
+my $mail_id_status_n2;
+my $mail_id_status_n3;
+my $ddelivery_status;
+
 
 
 open(FILE, "zimbra.log") or die "Couldn't open mail.log: $!; aborting";
@@ -18,29 +21,37 @@ $message_time = $1;
 $message_id = $2;
 $client_ip = $3;
 
-print "Message-ID: \t", $message_id,"\n";
-print "Message-Time: \t", $message_time,"\n";
-print "Client-IP: \t", $client_ip,"\n\n";
+# print "Message-ID: \t", $message_id,"\n";
+# print "Message-Time: \t", $message_time,"\n";
+# print "Client-IP: \t", $client_ip,"\n\n";
 } elsif ($_ =~ /.* postfix\/qmgr\[.*\]: (\S+): from=<(\S+)>, size=(\d+), nrcpt=(\d+)/) {
 $message_id = $1;
 $mail_from = $2;
 $message_size = $3;
 $nr_recipient = $4;
-
-print "Message-ID: \t", $message_id,	"\n";
-print "Mail from: \t",	$mail_from,	"\n";
-print "Messagesize: \t",	$message_size,	"\n";
-print "No recipients:\t",	$nr_recipient,	"\n\n";
 #} elsif ($_ =~ /.* postfix\/.*: (\S+): to=<(\S+)>/) {
 } elsif ($_ =~ /.* postfix\/.*: (\S+): to=<(\S+)>, relay=.*, delay=.*, delays=.*, dsn=.*, status=.*: 250 2.0.0 Ok: queued as (\S+)/) {
 $message_id = $1;
 $mail_to = $2;
-$mail_id_status = substr($3, 0,13);
+$mail_id_status_n2 = substr($3, 0,13);
+# }#Parser ID N3
+# elsif ($_ =~ /.* postfix\/.*: (\S+): to=<(\S+)>, relay=.*, delay=.*, delays=.*, dsn=.*, (\S+)/) {
+#  $message_id = $1;
+#  $mail_to = $2;
+#  $mail_id_status_n2 = $mail_id_status_n2;
+#  $mail_id_status_n3 = $3;
+#  print "Mail ID status N3: \t",	$mail_id_status_n3,	"\n";
 
-print "Message-ID: \t",	$message_id,	"\n";
+print "Message-ID: \t", $message_id,"\n";
+print "Message-Time: \t", $message_time,"\n";
+print "Client-IP: \t", $client_ip,"\n";
+print "Mail from: \t",	$mail_from,	"\n";
+print "Messagesize: \t",	$message_size,	"\n";
+print "No recipients:\t",	$nr_recipient,	"\n";
 print "Mail to: \t",	$mail_to,	"\n";
-print "Mail ID status: \t",	$mail_id_status,	"\n\n";
-
+print "Mail ID N2: \t",	$mail_id_status_n2,	"\n\n";
+# print "Mail ID N3: \t",	$mail_id_status_n3,	"\n\n";
+# print "Mail delivery status: \t",	$ddelivery_status,	"\n\n";
 }
 }
 close(FILE);
